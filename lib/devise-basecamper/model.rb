3012922b52d65be406c8733c45c6d90e810bbeac
JD Hendrickson <jd@digitalopera.com>
2012-09-20 01:26:12 -0500

    Added support for RECOVERABLE resources

--- lib/devise-basecamper/model.rb
+++ lib/devise-basecamper/model.rb
@@ -2,22 +2,22 @@ module Devise
   module Models
     module Basecamper
       extend ActiveSupport::Concern
-      
+
       module ClassMethods
         def devise_basecamper(opts={})
           defaults = {
             :subdomain_class  => :account,
             :subdomain_field  => :subdomain,
-            :scope_field      => :account_id            
+            :scope_field      => :account_id
           }
           @devise_basecamper_settings = defaults.merge(opts)
         end
-                
+
         def basecamper
           self.devise_basecamper if @devise_basecamper_settings.nil?
           return @devise_basecamper_settings
         end
-        
+
         def find_for_authentication(conditions={})
           if conditions[:subdomain].present?
               resource                                  = self.basecamper[:subdomain_class].to_s.camelize.constantize
@@ -26,6 +26,20 @@ module Devise
               conditions.delete(self.basecamper[:subdomain_field])
           end
           super
+        end
+
+        # TODO: Probably should break this out or atleast put some conditions in place
+        # so that this method is only included when "RECOVERABLE" is a specified option
+        # for the resource.
+        #
+        # We want to override this method to find our users within the scope
+        # of our subdomain.
+        def send_reset_password_instructions(attributes={})
+          resource          = self.basecamper[:subdomain_class].to_s.camelize.constantize
+          subdomain_source  = resource.to_adapter.find_first(self.basecamper[:subdomain_field] => attributes[:subdomain])
+          recoverable       = find_or_initialize_with_errors([self.basecamper[:scope_field], :email], { :email => attributes[:email], self.basecamper[:scope_field] => (subdomain_source.nil? ? nil : subdomain_source.id.to_s) })
+          recoverable.send_reset_password_instructions if recoverable.persisted?
+          recoverable
         end
       end
     end
--- lib/devise-basecamper/version.rb
+++ lib/devise-basecamper/version.rb
@@ -1,5 +1,5 @@
 module Devise
   module Basecamper
-    VERSION = "0.1.3"
+    VERSION = "0.2.0"
   end
 end
